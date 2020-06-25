5279319688b0d83b2d1151b20112798010c6796e
Declan Murphy <declxn@gmail.com>
2020-06-24 20:47:58 -0500

    Future Match maker code installed.

--- scratch1.rb
+++ scratch1.rb
@@ -1,7 +1,27 @@
-count = 0
+# count = 0
 
-while count < 11
-  p count
-  count += 1
+# while count < 11
+#   p count
+#   count += 1
+# end
+
+sam_cook = ["pasta", "guacamole", "sandwich", "ramen", "piano bread", "eggs", "buffalo chicken", "pb+j", "hot bread", "wet bread", "dry bread"]
+sally_lang = ["japanese", "croatian", "russian", "bingo", "love", "english"]
+
+# p sam_cook.length
+# p sally_lang
+
+
+if sam_cook.length > 10 && sally_lang.length > 5
+  puts "Hooee, y'all should date!"
+else
+  puts "Hoeee, bummer!"
 end
 
+#If Sam can cook more than 10 recipes and Sally 
+#speaks more than 5 languages, they should date. 
+#Based on the above arrays, print out a message 
+#describing whether or not they should date.
+#If Sam can make crepes or Sally can speak French, 
+#they should marry. Based on the above arrays, print 
+#out a message describing whether or not they should marry.
